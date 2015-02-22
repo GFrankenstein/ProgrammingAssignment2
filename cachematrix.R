5831bcd91a13ec6baadb7a869d420d8cf77db58a
G.Frankenstein <frankensteinyang@gmail.com>
2015-02-23 04:11:46 +0900

    update
    update

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
-## This program follows exactly as the example to create a special object that stores a matrix and caches its inverse. (The program and even the comments are also just some naive replacements of the example, OMG...)
-## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+## This program follows exactly as the example to create
+## a special object that stores a matrix and caches its inverse. 
+## (The program and even the comments are also just some naive replacements of the example, OMG...)
+## The first function, makeCacheMatrix creates a special "matrix", 
+## which is really a list containing a function to
 ##	1	set the value of the matrix
 ##	2	get the value of the matrix
 ##	3	set the value of the inverse matrix
@@ -20,7 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The following function calculates the inverse of the special "matrix" created with the above function. However, it first checks to see if the inverse matrix has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse matrix in the cache via the setinverse function.
+## The following function calculates the inverse of the special "matrix" created with the above function. 
+## However, it first checks to see if the inverse matrix has already been calculated. If so, it gets the 
+## inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the data and 
+## sets the value of the inverse matrix in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
